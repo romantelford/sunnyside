5a192925310073c667425edea4492113f9505300
romantelford <telford.roman@gmail.com>
2022-02-04 09:14:07 +1300

    Full upload

---
+++ js/_mobileMenu.js
@@ -0,0 +1,8 @@
+function mobileMenu() {
+    var menu = document.getElementById("mobileMenu");
+    if (menu.style.display === "flex") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "flex";
+    }
+}
